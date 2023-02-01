744a79da330eaac28813a307e8bd01e954f380dc
sdk17586 <122353362+sdk17586@users.noreply.github.com>
2023-02-01 17:22:17 +0900

    Update nsm.py

--- nsm.py
+++ nsm.py
@@ -93,7 +93,6 @@ st.set_page_config(layout="centered")
 
 with st.form(key ='Form1'):
     with st.sidebar:
-        
         select_language = st.sidebar.radio('데이터 분석 결과', ('금리와 집값 빠르게 파악하기', '야구 순위와 승률 빠르게 파악하기', '다른 데이터 분석'))
         
         
@@ -103,4 +102,4 @@ if select_language =='금리와 집값 빠르게 파악하기':
 
         
 elif select_language =='야구 순위와 승률 빠르게 파악하기':
-    bar_chart()
+     bar_chart()
