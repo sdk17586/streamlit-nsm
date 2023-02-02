4569dae82a85fa1c3ce537602d9778412852564b
sdk17586 <122353362+sdk17586@users.noreply.github.com>
2023-02-02 16:14:13 +0900

    Update nsm.py

--- nsm.py
+++ nsm.py
@@ -20,6 +20,10 @@ def  plotting_demo():
     st.write('You selected:', option)
 
     money = money[:] [money['A_YEAR']== option2]
+    
+    global  aa
+    
+    aa = money
 
     fig, ax = plt.subplots(2,2, figsize=(12,8))
 
@@ -97,10 +101,16 @@ with st.form(key ='Form1'):
         select_language = st.sidebar.radio('데이터 분석 결과', ('금리와 집값 빠르게 파악하기', '야구 순위와 승률 빠르게 파악하기', '다른 데이터 분석'))
         
 if select_language =='금리와 집값 빠르게 파악하기':           
-    try:
-          plotting_demo()  
-    except:      
-          pass
+    tab1, tab2 = st.tabs(["📈 Chart", "🗃 Data"])
+   
+    with tab1:
+        tab1.subheader("A tab with a chart")
+        plotting_demo()
+        
+    with tab2:
+        tab2.subheader("A tab with the data")
+        st.dataframe(aa)
+  
 
         
 elif select_language =='야구 순위와 승률 빠르게 파악하기':
